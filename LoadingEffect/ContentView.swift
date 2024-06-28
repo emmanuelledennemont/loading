fef3983de3c4f22e9d35bd942466d36f4abdf1b0
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-28 11:37:38 +0200

    content view

--- LoadingEffect/ContentView.swift
+++ LoadingEffect/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            Text("Hello la Team")
+                .foregroundColor(.white)
+                .font(.system(size: 30))
         }
-        .padding()
     }
 }
 
