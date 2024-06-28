7d909f606c5b8a92c4d9827b3e5c02ca756ef4ed
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-28 11:12:29 +0200

    refacto

---
+++ LoadingEffect/SplashScreenView.swift
@@ -0,0 +1,51 @@
+//
+//  SplashScreenView.swift
+//  SplashScreen
+//
+//  Created by Emmanuelle  Dennemont on 01/06/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State var isActive: Bool = false
+    @State private var size: CGFloat = 1.0
+    @State private var opacity: Double = 1.0
+
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                        .scaleEffect(size)
+                        .opacity(opacity)
+                        .onAppear {
+                            let baseAnimation = Animation.easeInOut(duration: 1.2)
+                            let repeated = baseAnimation.repeatForever(autoreverses: true)
+
+                            withAnimation(repeated) {
+                                self.size = 1.2
+                                self.opacity = 0.8
+                            }
+                        }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
