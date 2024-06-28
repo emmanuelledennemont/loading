cb9a96cc70850e7c8d4e7799f14b883cd8d3c02d
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-28 11:03:28 +0200

    Initial Commit

---
+++ LoadingEffect/LoadingEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingEffectApp.swift
+//  LoadingEffect
+//
+//  Created by Emmanuelle  Dennemont on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoadingEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
